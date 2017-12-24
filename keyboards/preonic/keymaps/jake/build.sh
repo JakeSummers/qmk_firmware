c2c7e2de244cce3f7a5be563207ef8f9ed51d825
Jake - Tide <summers@cs.dal.ca>
2017-12-24 18:19:40 -0500

    Good progress towards board layout I want.
    First layer done except for:
    * alt key
    * make raise a space on tap
    
    Raise layer has some progress but still needs work.
    
    * Need to figure out how to do double tap for brackets

---
+++ keyboards/preonic/keymaps/jake/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+figlet 'Build QMK Firmware'
+cd ~/repos/qmk_firmware
+make preonic:jake
+
+figlet 'Erase Board'
+sudo dfu-programmer atmega32u4 erase
+
+figlet 'Flash Board'
+sudo dfu-programmer atmega32u4 flash /home/jsummers/repos/qmk_firmware/preonic_rev2_jake.hex 
+
+figlet 'DONE'
