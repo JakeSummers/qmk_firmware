ca2af9fe837a733762c958fb33cc19e876ecab6d
Jake - Tide <summers@cs.dal.ca>
2020-08-02 16:48:58 -0400

    Switch up and down buttons to be the order of vim arrows

--- keyboards/preonic/keymaps/jake/keymap.c
+++ keyboards/preonic/keymaps/jake/keymap.c
@@ -79,7 +79,7 @@ KC_LGUI - Windows
   {KC_PGDOWN,     KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,        KC_BSPC,  KC_Y,        KC_U,    KC_I,    KC_O,    KC_P   },
   {KC_LCTRL, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,        KC_ENT,   KC_H,        KC_J,    KC_K,    KC_L,    KC_SCLN},
   {KC_LSFT,       KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,        KC_TAB,   KC_N,        KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
-  {KC_LALT, KC_LGUI, workup,  workdown, LOWER,   KC_SPC,      RAISE,    KC_SPC,      KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT}
+  {KC_LALT, KC_LGUI, workup,  workdown, LOWER,   KC_SPC,      RAISE,    KC_SPC,      KC_LEFT,    KC_DOWN, KC_UP, KC_RIGHT}
 },
 
 
