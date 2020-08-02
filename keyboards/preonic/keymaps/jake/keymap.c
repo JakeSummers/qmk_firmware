7126527cad4d0bf5c191197de9e23fcf3a912660
Jake - Tide <summers@cs.dal.ca>
2020-08-02 16:43:33 -0400

    Remove sticky keys
    Before when you tapped control or alt, they would remain active for some
    duration of time.  This was annoying.

--- keyboards/preonic/keymaps/jake/keymap.c
+++ keyboards/preonic/keymaps/jake/keymap.c
@@ -77,9 +77,9 @@ KC_LGUI - Windows
 [_QWERTY] = {
   {KC_PGUP,       KC_1,    KC_2,    KC_3,     KC_4,    KC_5,        KC_DEL,   KC_6,        KC_7,    KC_8,    KC_9,    KC_0   },
   {KC_PGDOWN,     KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,        KC_BSPC,  KC_Y,        KC_U,    KC_I,    KC_O,    KC_P   },
-  {OSM(MOD_LCTL), KC_A,    KC_S,    KC_D,     KC_F,    KC_G,        KC_ENT,   KC_H,        KC_J,    KC_K,    KC_L,    KC_SCLN},
+  {KC_LCTRL, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,        KC_ENT,   KC_H,        KC_J,    KC_K,    KC_L,    KC_SCLN},
   {KC_LSFT,       KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,        KC_TAB,   KC_N,        KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
-  {OSM(MOD_LALT), KC_LGUI, workup,  workdown, LOWER,   KC_SPC,      RAISE,    KC_SPC,      KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT}
+  {KC_LALT, KC_LGUI, workup,  workdown, LOWER,   KC_SPC,      RAISE,    KC_SPC,      KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT}
 },
 
 
