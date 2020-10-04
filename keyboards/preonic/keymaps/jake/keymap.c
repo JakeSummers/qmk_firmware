236546850ff74961f6aee815a9a014f7f0bb2867
Jake - Tide <summers@cs.dal.ca>
2020-10-04 12:06:25 -0400

    Shift the OS Key two spots to the right

--- keyboards/preonic/keymaps/jake/keymap.c
+++ keyboards/preonic/keymaps/jake/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|------+------+------+------+------|
  * | Ctrl |   Z  |   X  |   C  |   V  |CTL(B)| Tab  |ALT(N)|   M  |   ,  |   .  |   /  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Alt  |  OS  |      |      | Lower| Spac | Raise| Spac | Left | Up   | Down | Right|
+ * | Alt  |      |      |  OS  | Lower| Spac | Raise| Spac | Left | Up   | Down | Right|
  * `-----------------------------------------------------------------------------------'
 
 KC_LALT - Alt
@@ -79,9 +79,8 @@ KC_LGUI - Windows
   {KC_PGDOWN,     KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,        KC_BSPC,  KC_Y,        KC_U,    KC_I,    KC_O,    KC_P   },
   {KC_LCTRL, KC_A,    KC_S,    KC_D,     KC_F,    KC_G,        KC_ENT,   KC_H,        KC_J,    KC_K,    KC_L,    KC_SCLN},
   {KC_LSFT,       KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,        KC_TAB,   KC_N,        KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
-  {KC_LALT, KC_LGUI, workup,  workdown, LOWER,   KC_SPC,      RAISE,    KC_SPC,      KC_LEFT,    KC_DOWN, KC_UP, KC_RIGHT}
+  {KC_LALT,  workup,  workdown, KC_LGUI, LOWER,   KC_SPC,      RAISE,    KC_SPC,      KC_LEFT,    KC_DOWN, KC_UP, KC_RIGHT}
 },
-
 
 
 
